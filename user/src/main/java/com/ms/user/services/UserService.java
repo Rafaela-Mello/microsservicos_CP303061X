fc8c94cca08ad265c2592c7adb2fead0754de242
Rafaela Mello <rafaelamello2404@gmail.com>
2026-03-13 20:44:56 -0300

    adding new project

---
+++ user/src/main/java/com/ms/user/services/UserService.java
@@ -0,0 +1,28 @@
+package com.ms.user.services;
+
+import org.springframework.stereotype.Service;
+
+import com.ms.user.models.UserModel;
+import com.ms.user.producers.UserProducer;
+import com.ms.user.repositories.UserRepository;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final UserProducer userProducer;
+
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
+        this.userRepository = userRepository;
+        this.userProducer = userProducer;
+    }
+
+    @Transactional
+    public UserModel save(UserModel userModel) {
+        userModel = userRepository.save(userModel);
+        userProducer.publishMessageEmail(userModel);
+        return userModel;
+    }
+}
